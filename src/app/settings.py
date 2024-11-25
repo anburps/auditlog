4be53943f52268f1ca079a2660626c1dab97fb85
anburps <anbalaganrps@gmail.com>
2024-11-25 09:36:00 +0530

    url configuration

--- src/app/settings.py
+++ src/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'auditapp',
+    'auditlog_app',
+
 ]
 
 MIDDLEWARE = [
